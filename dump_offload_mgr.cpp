62ffa8338b1e85ae97baa818ad83450c919eac58
Marri Devender Rao <devenrao@in.ibm.com>
2022-02-15 05:32:53 -0600

    Check for system is HMC managed and host is in standby or running
    1)Do not start the offload handler if system is hmc managed system.
    Changing system from HMC managed to Non hmc is disruptive process
    and requires REIPL
    
    2) Check for system is hmc managed before any offload as system
    can changed from Non HMC managed system to HMC managed at runtime.
    
    3) If host is not at standby/runtime do not create the offload
    handlers rather add watch on host state property.
    
    3) When the host state property changes to standby/runtime create the
    offload handlers and initiate the offload.
    
    Tested: offlaoding after host goes to running state
    pvm_dump_offload[14222]: Host is not running do not offload dump, add watch on host state
    pvm_dump_offload[17032]: Host state propertiesChanged interface (xyz.openbmc_project.State.Boot.Progress)
    pvm_dump_offload[17032]: Offloading dumps host is now in (xyz.openbmc_project.State.Boot.Progress.ProgressStages.SystemInitComplete) state
    pvm_dump_offload[17032]: DumpOffloadManager::offload
    pvm_dump_offload[17032]: DumpOffloadHandler::offload entryType (xyz.openbmc_project.Dump.Entry.BMC) dumpType (0)
    pvm_dump_offload[17032]: DumpOffloadHandler::offload dump object (/xyz/openbmc_project/dump/bmc/entry/5)
    
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
    Change-Id: I1c0dd9ae5f69719f9519d15cab122d85fa6bb3d3

--- dump_offload_mgr.cpp
+++ dump_offload_mgr.cpp
@@ -10,35 +10,101 @@ DumpOffloadManager::DumpOffloadManager(sdbusplus::bus::bus& bus) : _bus(bus)
 {
     // add bmc dump offload handler to the list of dump types to offload
     std::unique_ptr<DumpOffloadHandler> bmcDump =
-        std::make_unique<DumpOffloadHandler>(bus, bmcEntryIntf, DumpType::bmc);
+        std::make_unique<DumpOffloadHandler>(_bus, bmcEntryIntf, DumpType::bmc);
     _dumpOffloadList.push_back(std::move(bmcDump));
 
     // add host dump offload handler to the list of dump types to offload
     std::unique_ptr<DumpOffloadHandler> hostbootDump =
-        std::make_unique<DumpOffloadHandler>(bus, hostbootEntryIntf,
+        std::make_unique<DumpOffloadHandler>(_bus, hostbootEntryIntf,
                                              DumpType::hostboot);
     _dumpOffloadList.push_back(std::move(hostbootDump));
 
     // add sbe dump offload handler to the list of dump types to offload
     std::unique_ptr<DumpOffloadHandler> sbeDump =
-        std::make_unique<DumpOffloadHandler>(bus, sbeEntryIntf, DumpType::sbe);
+        std::make_unique<DumpOffloadHandler>(_bus, sbeEntryIntf, DumpType::sbe);
     _dumpOffloadList.push_back(std::move(sbeDump));
 
-    // add hardware dump offload handler to the list of dump types to offload
+    // add hardware dump offload handler to the list of dump types to
+    // offload
     std::unique_ptr<DumpOffloadHandler> hardwareDump =
-        std::make_unique<DumpOffloadHandler>(bus, hardwareEntryIntf,
+        std::make_unique<DumpOffloadHandler>(_bus, hardwareEntryIntf,
                                              DumpType::hardware);
     _dumpOffloadList.push_back(std::move(hardwareDump));
 }
 
+void DumpOffloadManager::propertiesChanged(sdbusplus::message::message& msg)
+{
+    std::string intf;
+    DBusPropertiesMap propMap;
+    msg.read(intf, propMap);
+    log<level::INFO>(
+        fmt::format("Host state propertiesChanged interface ({}) ", intf)
+            .c_str());
+    for (auto prop : propMap)
+    {
+        if (prop.first == "BootProgress")
+        {
+            auto progress = std::get_if<std::string>(&prop.second);
+            if (progress != nullptr)
+            {
+                ProgressStages bootProgress =
+                    sdbusplus::xyz::openbmc_project::State::Boot::server::
+                        Progress::convertProgressStagesFromString(*progress);
+                if ((bootProgress == ProgressStages::SystemInitComplete) ||
+                    (bootProgress == ProgressStages::OSStart) ||
+                    (bootProgress == ProgressStages::OSRunning))
+                {
+                    log<level::INFO>(
+                        fmt::format("Offloading dumps host is now in  ({})"
+                                    " state ",
+                                    *progress)
+                            .c_str());
+                    offloadHelper();
+                }
+            }
+        }
+    }
+}
+
+void DumpOffloadManager::offloadHelper()
+{
+    // ensure dumps are offloaded only once, also hmc might change from
+    // non hmc to hmc by the time host goes to runtime so add HMC state check
+    if (!_fOffloaded && !isSystemHMCManaged(_bus))
+    {
+        // we can query only on the dump service not on individual entry types,
+        // so we get dumps of all types
+        ManagedObjectType objects = openpower::dump::getDumpEntries(_bus);
+        for (auto& dump : _dumpOffloadList)
+        {
+            dump->offload(objects);
+        }
+        _fOffloaded = true;
+    }
+}
+
 void DumpOffloadManager::offload()
 {
-    // we can query only on the dump service not on individual entry types,
-    // so we get dumps of all types
-    ManagedObjectType objects = openpower::dump::getDumpEntries(_bus);
-    for (auto& dump : _dumpOffloadList)
+    if (isHostRunning(_bus))
     {
-        dump->offload(objects);
+        offloadHelper();
+    }
+    else
+    {
+        // Perform offload only if host is in running state, else add watch on
+        // BootProgress property and offload when it moves to running state.
+        //
+        // At present we do not have a system-d target which indicates host is
+        // in running target with which we could have added wait-for condition
+        // for this service to start.
+        log<level::ERR>("Host is not in running state adding watch"
+                        " on boot progress property");
+        _hostStatePropWatch = std::make_unique<sdbusplus::bus::match_t>(
+            _bus,
+            sdbusplus::bus::match::rules::propertiesChanged(
+                "/xyz/openbmc_project/state/host0",
+                "xyz.openbmc_project.State.Boot.Progress"),
+            [this](auto& msg) { this->propertiesChanged(msg); });
     }
 }
 } // namespace openpower::dump
