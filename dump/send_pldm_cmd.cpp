e707e7977d1852955a18f13f96c3fd7df511a4ce
SwethaParasa <parasa.swetha1@ibm.com>
2024-06-10 04:57:37 -0500

    dump: support for dump Offload to OS
    Changing Entry Interface and Object Path for sbe, hostboot and hardware
    dumps to accomodate new design.
    
    Change-Id: Ie05ed44c78bac21c8ff808b9d9aec63214259228
    Signed-off-by: SwethaParasa <parasa.swetha1@ibm.com>

--- dump/send_pldm_cmd.cpp
+++ dump/send_pldm_cmd.cpp
@@ -6,6 +6,7 @@
 #include <libpldm/file_io.h>
 
 #include <phosphor-logging/log.hpp>
+#include <sstream>
 
 namespace openpower::dump::pldm
 {
@@ -15,20 +16,22 @@ using ::phosphor::logging::log;
 void sendNewDumpCmd(uint32_t dumpId, DumpType dumpType, uint64_t dumpSize)
 {
     uint32_t pldmDumpType = 0;
+    std::stringstream ss;
+    ss << std::hex << std::uppercase << dumpId;
+    std::string dumpIdString = ss.str();
     // TODO https://github.com/ibm-openbmc/powervm-handler/issues/9
     switch (dumpType)
     {
         case DumpType::bmc:
             pldmDumpType = 0xF; // PLDM_FILE_TYPE_BMC_DUMP
             break;
-        case DumpType::sbe:
-            pldmDumpType = 0x10; // PLDM_FILE_TYPE_SBE_DUMP
-            break;
-        case DumpType::hostboot:
-            pldmDumpType = 0x11; // PLDM_FILE_TYPE_HOSTBOOT_DUMP
-            break;
-        case DumpType::hardware:
-            pldmDumpType = 0x12; // PLDM_FILE_TYPE_HARDWARE_DUMP
+        case DumpType::system:
+            if (dumpIdString.c_str()[0] == '3')
+                pldmDumpType = 0x10; // PLDM_FILE_TYPE_SBE_DUMP
+            else if (dumpIdString.c_str()[0] == '2')
+                pldmDumpType = 0x11; // PLDM_FILE_TYPE_HOSTBOOT_DUMP
+            else if (dumpIdString.c_str()[0] == '0')
+                pldmDumpType = 0x12; // PLDM_FILE_TYPE_HARDWARE_DUMP
             break;
         default:
             std::string err = fmt::format("Unsupported dump type ({}) ",
